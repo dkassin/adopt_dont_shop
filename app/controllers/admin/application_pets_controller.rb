40da8d1c7252e1b99c181a35b13eac8b94482a75
David Kassin <david.j.kassin@gmail.com>
2021-12-14 17:31:06 -0700

    incompleted reject application

--- app/controllers/admin/application_pets_controller.rb
+++ app/controllers/admin/application_pets_controller.rb
@@ -5,4 +5,10 @@ class Admin::ApplicationPetsController < ApplicationController
     redirect_to "/admin/applications/#{@application.id}"
   end
 
+  def reject
+    @application_reject = ApplicationPet.find(params[:pet_id])
+    @application_reject.update({:approved => "Rejected"})
+    redirect_to "/admin/applications/#{@application.id}"
+  end
+
 end
--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,6 +1,5 @@
 class Admin::ApplicationsController < ApplicationController
   def show
-
     @application = Application.all[0]
   end
 
